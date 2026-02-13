96970b70162a12cd2bf70145c7c261c43b48dfe5
Mark_Deng <markdeng@duck.com>
2026-02-13 23:11:24 +0800

    fix: complete ESM transition in serverless entry point

--- api/serverless.js
+++ api/serverless.js
@@ -1,19 +1,13 @@
-"use strict";
-
-// Read the .env file.
 import * as dotenv from "dotenv";
 dotenv.config();
-
-// Require the framework
 import Fastify from "fastify";
+import appInstance from "../app.js";
 
-// Instantiate Fastify with some config
 const app = Fastify({
   logger: true,
 });
 
-// Register your application as a normal plugin.
-app.register(import("../app.js"));
+app.register(appInstance);
 
 export default async (req, res) => {
   await app.ready();
